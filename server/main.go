b01c233dd90eb703111ca5cf6818237715571fd9
Finley Ge <m13203533462@163.com>
2022-01-16 08:10:15 +0800

    router init

--- server/main.go
+++ server/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "zjutjh/Join-Us/utility"
+import (
+	"zjutjh/Join-Us/router"
+	"zjutjh/Join-Us/utility"
+)
 
 func main() {
     utility.ConfigInit() // 配置文件初始化
     utility.DatabaseInit() // 服务器初始化
+    router.Init() // 路由初始化
+    utility.RunServer() // 运行服务器
 }
