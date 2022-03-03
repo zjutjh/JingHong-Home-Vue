2633a3b3a3de40813789f8dc58835aa1b632972a
F1nley <m13203533462@163.com>
2022-03-03 20:36:20 +0800

    delete vue

--- join-us-vue/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './routes/index'
-import {createPinia} from 'pinia'
-createApp(App).
-use(router).
-use(createPinia()).
-mount('#app')
--- join-us-vue/src/types/api.ts
+++
@@ -1,4 +0,0 @@
-export interface ResponseType<T = any> {
-    message: string;
-    data: T;
-}
